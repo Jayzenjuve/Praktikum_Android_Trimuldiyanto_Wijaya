b7d8e139cded5fb3d54e157d9517b69821f59d82
Jayz _ <jayabbm55@gmail.com>
2024-12-14 15:18:05 +0800

    Pertemuan Terakhir

---
+++ lib/api/service.dart
@@ -0,0 +1,16 @@
+import 'package:aplikasi/login.dart';
+import 'package:dio/dio.dart';
+
+class DataService{
+  final String _url = "http://localhost:8000/api/";
+
+  Dio dio = Dio();
+
+  LoginService(String username, String password) async {
+    final response = await dio.post(_url + 'login', data: {
+      'username': username,
+      'password': password,
+    });
+    return response.data;
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi/api/service.dart';
 import 'package:aplikasi/dashboard.dart';
 import 'package:flutter/material.dart';
 import "package:get/get.dart";
@@ -10,6 +11,22 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+
+  final _formState = GlobalKey<FormState>();
+  TextEditingController _user = TextEditingController();
+  TextEditingController _pass = TextEditingController();
+
+  Future _masuk() async {
+    var response = await DataService().LoginService(_user.text, _pass.text);
+  if (response == true){
+    Get.offAll(Dashboard());
+  } else {
+    Get.defaultDialog(
+      title: "User/Pass Salah",
+    );
+  }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,55 +64,74 @@ class _loginState extends State<login> {
         ),
         body: Padding(
           padding: EdgeInsets.all(50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 100,
-                backgroundImage: NetworkImage(
-                    'https://cdn.7tv.app/emote/01GESA72SG000B6WHR50T3PZD4/4x.webp'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0)),
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person)),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0)),
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock)),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.offAll(Dashboard());
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50),
-                  backgroundColor: Colors.blueAccent,
-                  foregroundColor: Colors.black,
-                  shadowColor: Colors.black,
-                  elevation: 5,
+          child: Form(
+            key: _formState,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: NetworkImage(
+                      'https://cdn.7tv.app/emote/01GESA72SG000B6WHR50T3PZD4/4x.webp'),
                 ),
-                child: Text(
-                  'MASUK',
-                  style: TextStyle(color: Colors.white),
+                SizedBox(
+                  height: 20,
                 ),
-              )
-            ],
+                TextFormField(
+                  validator: (value) {
+                    if (value == ''){
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  controller: _user,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == ''){
+                      return "Password tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                  controller: _pass,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock)),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formState.currentState!.validate()){
+                      _masuk();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                    backgroundColor: Colors.blueAccent,
+                    foregroundColor: Colors.black,
+                    shadowColor: Colors.black,
+                    elevation: 5,
+                  ),
+                  child: Text(
+                    'MASUK',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
